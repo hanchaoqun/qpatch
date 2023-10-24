55de5aee69aaa5f10f84362e5ec621d9d4a0d298
Chaoqun Han <74887456+hanchaoqun@users.noreply.github.com>
2023-10-24 11:09:46 +0300

    Create opcode.h

---
+++ opcode.h
@@ -0,0 +1,20 @@
+//===----------------------------------------------------------------------===//
+//
+// Copyright 2023 hanssccv@gmail.com. All rights reserved.
+// Use of this source code is governed by a Anti-996 style
+// license that can be found in the LICENSE file.
+//
+//===----------------------------------------------------------------------===//
+#ifndef __HPATCH_OPCODE_H__
+#define __HPATCH_OPCODE_H__
+
+#include "dopra.h"
+
+
+extern unsigned long get_opcode_size_64(unsigned char* startaddress);
+
+#define get_opcode_size(a) get_opcode_size_64((a))
+
+
+#endif /* __HPATCH_OPCODE_H__ */
+
