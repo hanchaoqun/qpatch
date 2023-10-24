7c04e70884ed8d360db2d99d9c97cd8a7cd17819
Chaoqun Han <74887456+hanchaoqun@users.noreply.github.com>
2023-10-24 11:10:02 +0300

    Create opcode.c

---
+++ opcode.c
@@ -0,0 +1,32 @@
+//===----------------------------------------------------------------------===//
+//
+// Copyright 2023 hanssccv@gmail.com. All rights reserved.
+// Use of this source code is governed by a Anti-996 style
+// license that can be found in the LICENSE file.
+//
+//===----------------------------------------------------------------------===//
+#include "opcode.h"
+#include "distorm.h"
+
+#define DECODE_TYPE Decode64Bits
+
+unsigned long get_opcode_size_64(unsigned char* startaddress)
+{
+    _DecodedInst decodeResult[50];
+    unsigned int uiCount = 0;
+    _DecodeResult ret;
+
+    unsigned char* pbyte = startaddress;
+
+    ret = distorm_decode(0,pbyte,50, Decode64Bits,
+        decodeResult, 50, &uiCount);
+    
+    if ( ret != DECRES_SUCCESS )
+    {
+        printf("distorm_decode64 error!\n");
+        return 0;
+    }
+    return decodeResult[0].size;
+}
+
+
