cc403f3cae93d891020fa6191561a4a18376552b
Chaoqun Han <74887456+hanchaoqun@users.noreply.github.com>
2024-07-25 17:29:50 +0300

    format coding style

--- define.c
+++ define.c
@@ -7,60 +7,50 @@
 //===----------------------------------------------------------------------===//
 #include "define.h"
 
-
 UINT32 g_ucurLogLevel = LOG_ERR;
 
-UINT32 LOG_TextAdapt (UINT32 enLevel,
-                          UINT32 ulModule,
-                          UINT32 enLogType,
-                          CHAR* pMessage,
-                          ...)
-{
-    if (enLevel < g_ucurLogLevel)
-    {
-        return OK;
-    }
-
-    va_list     varPara;
-    INT32   lOffset = 0;
-    CHAR    logText[MAX_TRACE_LEN];
-    time_t      now = time(0);
-    struct tm*  tnow = NULL;
-
-    memset(logText, 0x00, MAX_TRACE_LEN);
-
-    time(&now);
-    tnow = localtime(&now);
-    lOffset = snprintf(logText, MAX_TRACE_LEN - 1, "%04d_%02d_%02d %02d:%02d:%02d ",
-                       1900 + tnow->tm_year, tnow->tm_mon + 1, tnow->tm_mday, tnow->tm_hour, tnow->tm_min, tnow->tm_sec);
-
-    va_start(varPara, pMessage);
-    lOffset += vsnprintf(logText + lOffset, MAX_TRACE_LEN - 1, pMessage, varPara);
-    va_end(varPara);
-
-    if (lOffset < (MAX_TRACE_LEN - 1))
-    {
-        logText[lOffset] = '\n';
-    }
-
-    printf("%s", logText);
-
+UINT32 LOG_TextAdapt(UINT32 enLevel, UINT32 ulModule, UINT32 enLogType,
+                     CHAR* pMessage, ...) {
+  if (enLevel < g_ucurLogLevel) {
     return OK;
+  }
+
+  va_list varPara;
+  INT32 lOffset = 0;
+  CHAR logText[MAX_TRACE_LEN];
+  time_t now = time(0);
+  struct tm* tnow = NULL;
+
+  memset(logText, 0x00, MAX_TRACE_LEN);
+
+  time(&now);
+  tnow = localtime(&now);
+  lOffset =
+      snprintf(logText, MAX_TRACE_LEN - 1, "%04d_%02d_%02d %02d:%02d:%02d ",
+               1900 + tnow->tm_year, tnow->tm_mon + 1, tnow->tm_mday,
+               tnow->tm_hour, tnow->tm_min, tnow->tm_sec);
+
+  va_start(varPara, pMessage);
+  lOffset += vsnprintf(logText + lOffset, MAX_TRACE_LEN - 1, pMessage, varPara);
+  va_end(varPara);
+
+  if (lOffset < (MAX_TRACE_LEN - 1)) {
+    logText[lOffset] = '\n';
+  }
+
+  printf("%s", logText);
+
+  return OK;
 }
 
-char* trimstr(char* str)
-{
-    int len, i;
-    len = strlen(str);
-    for (i = 0; i < len; i++)
-    {
-        if (str[i] == '\n' || str[i] == '\r')
-        {
-            str[i] = '\0';
-            i = len + 1;
-        }
+char* trimstr(char* str) {
+  int len, i;
+  len = strlen(str);
+  for (i = 0; i < len; i++) {
+    if (str[i] == '\n' || str[i] == '\r') {
+      str[i] = '\0';
+      i = len + 1;
     }
-    return str;
+  }
+  return str;
 }
-
-
